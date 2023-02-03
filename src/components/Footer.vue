26d5133ddd81c502d23a416b882a32f16004f9a4
juanamolinalucia2001 <juana2001molina@gmail.com>
2023-02-03 17:44:04 -0300

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+       <footer>
+        njdnchjbchbchbdbdb
+       </footer>
+    </div>
+</template>
+<script>
+export default {
+    name:'Footer'
+}
+</script>
+<style>
+footer {
+    position: fixed;
+    height: 100px;
+    bottom: 0;
+    width: 100%;
+    background: black;
+    color: white;
+}
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="dashboard">
+        <Header/>
+    <div class="container">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Dni</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Email</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="pacient of listPacient" :key="pacient.pacient">
+                <th scope="row">{{pacient.PacienteId}}</th>
+                <td>{{pacient.Nombre}}</td>
+                <td>{{pacient.DNI}}</td>
+                <td>{{pacient.Telefono}}</td>
+                <td>{{pacient.Correo}}</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
+        <Footer/>
+        
+    </div>
+</template>
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import axios from 'axios'
+
+export default {
+    name:"Dashboard",
+    data(){
+        return{
+            listPacient:null,
+            page:1
+        }
+    },
+    components:{
+        Header,
+        Footer
+    },
+    mounted(){
+        let api ="https://api.solodata.es/pacientes?page=" + this.pagina;
+        axios.get(api).then( data =>{
+            this.listPacient = data.data;
+        })
+    }
+}
+</script>
+<style scope>
+
+</style>
